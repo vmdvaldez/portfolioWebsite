bb4807c2e6ffbd92ab4b9684844838ca6825665a
Vincent Valdez <vmdvaldez@hotmail.com>
2023-11-29 04:38:08 -0500

    Add Skills Icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,13 +1,18 @@
 import styles from "../styles/Skills.module.css"
 
+const IMAGE_PATH = "../src/assets/skillsicon/"
 
 function SkillTab({category, items}){
+
     return(
         <div className={styles.skillsTab}>
             <h2>{category}</h2>
             <ul>
                 {items.map(item=>{
-                    return <li key={item}><img src="#"/><div>{item}</div></li>
+                    return <li key={item.name}>
+                            {item.img ? <img src={ IMAGE_PATH + item.img}/> : <img src="#"/>}
+                            <div>{item.name}</div>
+                            </li>
                 })}
             </ul>
         </div>
@@ -16,10 +21,29 @@ function SkillTab({category, items}){
 
 export default function SkillsSection(){
     const skills = {
-        "Front-End": ['HTML', 'CSS', 'JavaScript', 'React'],
-        "Back-End": ["C", "C++", "Java", "Python", "Node"],
-        "Cloud Tech": ["Docker", "Kubernetes", "OpenShift"],
-        "Other": ["MATLAB", "Wireshark", "Photoshop"]
+        "Front-End": [
+            {name:'HTML', img:"html5-original.svg"}, 
+            {name:'CSS', img:"css3-original.svg"}, 
+            {name:'JavaScript', img:"javascript-original.svg"}, 
+            {name:'React', img:"react-original.svg"}
+        ],
+        "Back-End": [
+            {name:"C", img:"c-original.svg"}, 
+            {name:"C++", img:"cplusplus-original.svg"}, 
+            {name:"Java", img:"java-original.svg"}, 
+            {name:"Python", img:"python-original.svg"}, 
+            {name:"NodeJs", img:"nodejs-original.svg"}
+        ],
+        "Cloud Tech": [
+            {name:"Docker", img:"docker-original.svg"}, 
+            {name:"Kubernetes", img:"kubernetes-plain.svg"}, 
+            {name:"OpenShift", img:".svg"}
+        ],
+        "Other": [
+            {name:"MATLAB", img:"matlab-original.svg"}, 
+            {name:"Wireshark", img:".svg"}, 
+            {name:"Photoshop", img:"photoshop-plain.svg"}
+        ]
     }
 
     return(
