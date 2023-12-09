dc34967bb8008a5271ecf03db7a58a5cea83e528
Vincent Valdez <vmdvaldez@hotmail.com>
2023-12-09 04:29:45 -0500

    Fix ContactMe Form to use fetch

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,49 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import styles from "../styles/Contact.module.css";
 
 export default function ContactSection({refProp}){
 
+    function submitToEmail(e){
+            e.preventDefault();
+            const email = e.target.elements.email.value;
+            const name = e.target.elements.name.value;
+            const message = e.target.elements.message.value;
+            fetch("https://formsubmit.co/ajax/vmdvaldez@hotmail.com", {
+                method: "POST",
+                headers: { 
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: name,
+                    email: email,
+                    message: message
+                })
+            })
+                .then(response => response.json())
+                .then(data => console.log(data))
+                .catch(error => console.log(error));
+                setSubmitted(true)
+    }
+
+    const [submitted, setSubmitted] = useState(false);
+
     return(
         <section className={styles.contactInfo} ref={refProp} id="Contact">
+            
             <h1>Contact Me</h1>
-            <form action="https://formsubmit.co/vmdvaldez@hotmail.com" method="POST">
-                <div>
-                    <input type="text" name="name" placeholder="Name"></input>
+            <form onSubmit={submitToEmail}>
+                <div className={styles.inputs}>
+                      <input type="text" name="name" placeholder="Name"></input>
                     <input type="email" name="email" placeholder="Email"></input>
                 </div>
                 <textarea type="text" name="message" placeholder="Type Your Message Here..."></textarea>
-                <button type="submit"><div>Submit</div></button>
+                {!submitted ? 
+                    <button type="submit">
+                        <div>Submit</div>
+                    </button> :
+                    <div className={styles.submitted}> Submitted</div>
+                }
             </form>
         </section>
     )
