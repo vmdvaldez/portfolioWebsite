f4f011dedc60284b4edb985fe4f9b02dee71a5b3
Vincent Valdez <vmdvaldez@hotmail.com>
2023-12-09 03:31:34 -0500

    add form submission emailing function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,12 +6,13 @@ export default function ContactSection({refProp}){
     return(
         <section className={styles.contactInfo} ref={refProp} id="Contact">
             <h1>Contact Me</h1>
-            <form>
+            <form action="https://formsubmit.co/vmdvaldez@hotmail.com" method="POST">
                 <div>
                     <input type="text" name="name" placeholder="Name"></input>
                     <input type="email" name="email" placeholder="Email"></input>
                 </div>
                 <textarea type="text" name="message" placeholder="Type Your Message Here..."></textarea>
+                <button type="submit"><div>Submit</div></button>
             </form>
         </section>
     )
